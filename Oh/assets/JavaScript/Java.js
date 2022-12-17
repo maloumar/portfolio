92d4531899c3724ba7f79f6c44353c450d05f95b
maloumar <95297523+maloumar@users.noreply.github.com>
2022-12-17 18:54:42 +0100

    17.12.2022

---
+++ Oh/assets/JavaScript/Java.js
@@ -0,0 +1,11 @@
+/* L'animation des boutons */
+document.getElementById('actif').onmousemove = (e) => {
+    const x = e.pageX - e.target.offsetLeft
+    const y = e.pageY - e.target.offsetTop
+
+    console.log(x + '/' + y);
+
+    e.target.style.setProperty('--x', `${x}px`)
+    e.target.style.setProperty('--y', `${y}px`)
+    console.log(e);
+}
