44a7b0aa362a24af2a2674ff7c95dcd8636b4dc6
maloumar <95297523+maloumar@users.noreply.github.com>
2022-12-17 19:55:14 +0100

    17.12.2022

--- Oh/assets/JavaScript/Java.js
+++ Oh/assets/JavaScript/Java.js
@@ -1,5 +1,5 @@
 /* L'animation des boutons */
-document.getElementById('actif').onmousemove = (e) => {
+document.getElementById('actif-menu').onmousemove = (e) => {
     const x = e.pageX - e.target.offsetLeft
     const y = e.pageY - e.target.offsetTop
 
